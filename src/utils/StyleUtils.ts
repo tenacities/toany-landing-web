fb963c2b4c740578032ee68ce2108ea9e247e347
wns25678 <wns25678@naver.com>
2022-03-08 10:28:14 +0900

    rfc: delete debugging log

--- src/utils/StyleUtils.ts
+++ src/utils/StyleUtils.ts
@@ -4,8 +4,5 @@
  * @param {number} scale 스케일
  */
 export function scaleDom(dom: HTMLElement, scale: number) {
-  if (scale == 1) {
-    console.log("!!!!!");
-  }
   dom.style.transform = `scale(${scale})`;
 }
