03f80f12265425eb8f944fbf43e562df77316c03
wns25678 <wns25678@naver.com>
2022-03-08 13:54:54 +0900

    ftr: apply 'adobe gothic' font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
       "./src/**/*.{ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'adobe': ['adobe-gothic-std', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
